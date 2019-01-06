99a4edbcd7183af46157935633872bab687779ca
NeonNP <rene.krueger94@gmail.com>
2019-01-06 19:02:45 +0100

    place popup in body and not in form

--- injectAddToCart.js
+++ injectAddToCart.js
@@ -8,12 +8,9 @@ popup.innerHTML += '<h2>Hast du bereits ein ähnliches Produkt?</h2>';
 popup.innerHTML += '<textarea rows="4" cols="50"/>';
 popup.innerHTML += '<h2>Wie lange wirst du das Produkt voraussichtlich verwenden?</h2>';
 popup.innerHTML += '<textarea rows="4" cols="50"/>';
-popup.innerHTML += '<div id="addToCart" class="a-button-stack"><span id="submit.add-to-cart" class="a-button a-spacing-small a-button-primary a-button-icon"><span class="a-button-inner"><i class="a-icon a-icon-cart"></i><input id="add-to-cart-button" name="submit.add-to-cart" type="submit" title="In den Einkaufswagen" data-hover="Wählen Sie <b>__dims__</b> auf der linken Seite<br> zum Hinzufügen zum Einkaufswagen" class="a-button-input" type="button" value="In den Einkaufswagen" aria-labelledby="submit.add-to-cart-announce"><span id="submit.add-to-cart-announce" class="a-button-text" aria-hidden="true">In den Einkaufswagen</span></span></span></div>';
+popup.innerHTML += '<div id="btn_addToCart" class="a-button-stack"><span id="submit.add-to-cart" class="a-button a-spacing-small a-button-primary a-button-icon"><span class="a-button-inner"><i class="a-icon a-icon-cart"></i><input id="add-to-cart-button" name="submit.add-to-cart" type="submit" form="addToCart" title="In den Einkaufswagen" data-hover="Wählen Sie <b>__dims__</b> auf der linken Seite<br> zum Hinzufügen zum Einkaufswagen" class="a-button-input" type="button" value="In den Einkaufswagen" aria-labelledby="submit.add-to-cart-announce"><span id="submit.add-to-cart-announce" class="a-button-text" aria-hidden="true">In den Einkaufswagen</span></span></span></div>';
 
-var addToCartForm = document.getElementById('addToCart');
-if(addToCartForm != null) {
-  addToCartForm.appendChild(popup);
-}
+document.body.appendChild(popup);
 
 var ogAddToCartBtn = document.getElementById('add-to-cart-button');
 
