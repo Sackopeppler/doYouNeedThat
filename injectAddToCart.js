63c75777e6b0b7352101d34b0a389aee2103a7a4
NeonNP <rene.krueger94@gmail.com>
2019-01-13 09:53:03 +0100

    add select boxes

--- injectAddToCart.js
+++ injectAddToCart.js
@@ -2,12 +2,25 @@ var popup = document.createElement('dialog');
 popup.id = 'popupNeedThat';
 popup.innerHTML =  '<a href="javascript:void(0)" class="exit" onclick="popup.close()"></a>';
 popup.innerHTML += '<h1>Brauchst du das wirklich?</h1>';
-popup.innerHTML += '<h2>Wofür wirst du dieses Produkt verwenden?</h2>';
-popup.innerHTML += '<textarea rows="4" cols="50"/>';
-popup.innerHTML += '<h2>Hast du bereits ein ähnliches Produkt?</h2>';
-popup.innerHTML += '<textarea rows="4" cols="50"/>';
-popup.innerHTML += '<h2>Wie lange wirst du das Produkt voraussichtlich verwenden?</h2>';
-popup.innerHTML += '<textarea rows="4" cols="50"/>';
+popup.innerHTML +=    '<div class="dynt-qbox">'
+                    +   '<label for="tbWofuer">Wofür wirst du dieses Produkt verwenden?</label>'
+                    +   '<textarea id="tbWofuer" name="tbWofuer" rows="4" cols="50"></textarea>'
+                    + '</div>';
+popup.innerHTML +=    '<div class="dynt-qbox">'
+                    +   '<label for="aehnlichesProdukt">Hast du bereits ein ähnliches Produkt?</label>'
+                    +     '<select id="aehnlichesProdukt" name="aehnlichesProdukt">'
+                    +     '<option value="false" selected>Nein</option>'
+                    +     '<option value="true">Ja</option>'
+                    +   '</select>'
+                    + '</div>';
+popup.innerHTML +=    '<div class="dynt-qbox">'
+                    +   '<label for="selNutzungsdauer">Wie lange wirst du das Produkt voraussichtlich verwenden?</label>'
+                    +   '<select id="selNutzungsdauer" name="selNutzungsdauer">'
+                    +     '<option value="3" selected>Häufig</option>'
+                    +     '<option value="2">Ab und zu</option>'
+                    +     '<option value="1">Selten</option>'
+                    +     '<option value="0">Einmalig</option>'
+                    + '</div>';
 popup.innerHTML += '<div id="btn_addToCart" class="a-button-stack"><span id="submit.add-to-cart" class="a-button a-spacing-small a-button-primary a-button-icon"><span class="a-button-inner"><i class="a-icon a-icon-cart"></i><input id="add-to-cart-button" name="submit.add-to-cart" type="submit" form="addToCart" title="In den Einkaufswagen" data-hover="Wählen Sie <b>__dims__</b> auf der linken Seite<br> zum Hinzufügen zum Einkaufswagen" class="a-button-input" type="button" value="In den Einkaufswagen" aria-labelledby="submit.add-to-cart-announce"><span id="submit.add-to-cart-announce" class="a-button-text" aria-hidden="true">In den Einkaufswagen</span></span></span></div>';
 
 document.body.appendChild(popup);
